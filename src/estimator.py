e2bcf6996615a6aeed096a893004667f2090df39
CalvoM <mwadimemakokha@ieee.org>
2020-04-09 21:38:01 +0300

    Solve challenge one

--- src/estimator.py
+++ src/estimator.py
@@ -1,3 +1,10 @@
 def estimator(data):
+  impact = {}
+  severeImpact={}
+  impact.currentlyInfected =  data.reportedCases*10
+  severeImpact.currentlyInfected =  data.reportedCases*50
+  factor = data.timeToElapse//3
+  impact.infectionsByRequestedTime = impact.currentlyInfected*(2**factor)
+  severeImpact.infectionsByRequestedTime= severeImpact.currentlyInfected*(2**factor)
   print(data)
-  return data
+  return {data,impact,severeImpact}
