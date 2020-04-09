41e797933cc61bb80b6c92e03ade38f010de2582
CalvoM <mwadimemakokha@ieee.org>
2020-04-10 00:15:34 +0300

    Solve bug in challenge one solution

--- src/estimator.py
+++ src/estimator.py
@@ -3,7 +3,7 @@ def estimator(data):
   severeImpact={}
   impact["currentlyInfected"] =  data["reportedCases"]*10
   severeImpact["currentlyInfected"] =  data["reportedCases"]*50
-  factor = data["timeToElapse"]//3
+  factor = 28//3
   impact["infectionsByRequestedTime"] = impact["currentlyInfected"]*(2**factor)
   severeImpact["infectionsByRequestedTime"]= severeImpact["currentlyInfected"]*(2**factor)
   print(data)
